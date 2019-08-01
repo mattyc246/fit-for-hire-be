a18fa4d3f2c9595c91bd10c7e347dc32fbd1a2f4
Matthew Cross <mattyc246@gmail.com>
2019-08-01 16:21:06 +0800

    user can login in and return jwt

--- fitforhire_api/blueprints/sessions/views.py
+++ fitforhire_api/blueprints/sessions/views.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, request, jsonify, make_response
 from models.user import User
 from werkzeug.security import check_password_hash
+from flask_jwt_extended import create_access_token
 
-sessions_api_blueprint = Blueprint('sessions_api', __name__, template_folder='templates')
+sessions_api_blueprint = Blueprint(
+    'sessions_api', __name__, template_folder='templates')
+
 
 @sessions_api_blueprint.route('/', methods=['POST'])
 def create():
@@ -27,4 +30,12 @@ def create():
 
         return make_response(jsonify(response), 401)
 
-    
+    access_token = create_access_token(identity=user.id)
+
+    response = {
+        'message': 'Logged in successfully',
+        'valid': True,
+        'auth_token': access_token
+    }
+
+    return make_response(jsonify(response), 200)
