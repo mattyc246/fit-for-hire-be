a086a26b51e30caae2f90798ecbb05f1bb459322
Matthew Cross <mattyc246@gmail.com>
2019-08-06 17:50:18 +0800

    added expiry to jwt token

--- fitforhire_api/__init__.py
+++ fitforhire_api/__init__.py
@@ -3,6 +3,7 @@ from fitforhire_api.blueprints.sessions.views import sessions_api_blueprint
 from app import app
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
+from datetime import datetime, timedelta
 
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
@@ -12,3 +13,4 @@ cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 app.register_blueprint(users_api_blueprint, url_prefix='/api/v1/users')
 app.register_blueprint(sessions_api_blueprint, url_prefix='/api/v1/sessions')
 jwt = JWTManager(app)
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=30)
