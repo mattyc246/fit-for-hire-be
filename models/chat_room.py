795be03538eee93a126ae27ff242ecc2a2360712
Matthew Cross <mattyc246@gmail.com>
2019-08-13 16:31:18 +0800

    create chat room complete

--- models/chat_room.py
+++ models/chat_room.py
@@ -2,6 +2,8 @@ import peewee as pw
 from models.base_model import BaseModel
 from models.user import User
 
-class Chat(BaseModel):
+
+class ChatRoom(BaseModel):
     customer = pw.ForeignKeyField(User, backref="chats")
-    professional = pw.ForeignKeyField(User, backref=)
+    professional = pw.ForeignKeyField(User, backref="chats")
+    room_no = pw.CharField(null=False)
