0a45f577e2f167adbbb0bba3acc245b92430588d
Matthew Cross <mattyc246@gmail.com>
2019-08-13 22:58:32 +0800

    updated not to force host

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --worker-class eventlet -w 1 start:start
+web: gunicorn --worker-class eventlet -w 1 start:app
--- start.py
+++ start.py
@@ -2,4 +2,4 @@ from app import app, socketio
 import fitforhire_api
 import fitforhire_web
 
-socketio.run(app, host="0.0.0.0")
+socketio.run(app)
